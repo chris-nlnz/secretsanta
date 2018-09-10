3a6286a6afb9405104d1828ba21afd1cc4fb1a59
Chris van Egmond <chris.vanegmond@btg.co.nz>
2018-09-10 21:05:48 +1200

    Add home view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        return {'derp': 'lol'}
