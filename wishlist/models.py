f1621dc96d5fcfdf0a4ea44d5582cbc3fa9ab6f3
Chris van Egmond <chris.vanegmond@btg.co.nz>
2018-09-10 16:42:07 +1200

    Fix wishlist models and add migration and add wishlist to INSTALLED_APPS

--- wishlist/models.py
+++ wishlist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from secretsanta.utils import NULLABLE
 
+
 class Event(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
@@ -9,16 +10,18 @@ class Event(models.Model):
 
 
 class Participant(models.Model):
-    user = models.ForeignKey('django.contrib.auth.User')
-    event = models.ForeignKey('wishlist.Event')
-    buys_present_for = models.ForeignKey('wishlist.Participant')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    event = models.ForeignKey('wishlist.Event', on_delete=models.CASCADE)
+    buys_present_for = models.ForeignKey(
+        'wishlist.Participant', on_delete=models.SET_NULL, **NULLABLE
+    )
 
 
 class WishedItem(models.Model):
-    participant = models.ForeignKey('wishlist.Participant')
+    participant = models.ForeignKey('wishlist.Participant', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     url = models.URLField(**NULLABLE)
-    scratched_off_by = models.ForeignKey('wishlist.Participant')
-
-
-
+    scratched_off_by = models.ForeignKey(
+        'wishlist.Participant', related_name="scratched_offs",
+        on_delete=models.SET_NULL, **NULLABLE
+    )
