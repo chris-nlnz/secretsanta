ce23022eb596839c32e0a0ae153325b348ca8f1a
Chris van Egmond <chris.vanegmond@btg.co.nz>
2018-09-28 22:09:35 +1200

    Add wishlist models to django admin

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from wishlist.models import Event, Participant, WishedItem
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'date')
+    ordering = ('date', )
+admin.site.register(Event, EventAdmin)
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user', 'event', 'buys_present_for')
+    ordering = ('event__date', )
+admin.site.register(Participant, ParticipantAdmin)
+
+
+class WishedItemAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'participant', 'name', 'url', 'scratched_off_by')
+    ordering = ('participant__event__date', 'participant')
+admin.site.register(WishedItem, WishedItemAdmin)
