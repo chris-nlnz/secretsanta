841db6af6a5f3c9941f506d8f95ae7fa0c002174
Chris van Egmond <chris.vanegmond@btg.co.nz>
2018-11-18 02:51:56 +1300

    Add draw url to admin for ease

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from wishlist.models import Event, Participant, WishedItem
 
@@ -10,8 +11,19 @@ admin.site.register(Event, EventAdmin)
 
 
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'buys_present_for', 'user', 'event')
+    list_display = ('__str__', 'buys_present_for', 'user', 'event', 'draw_url')
     ordering = ('event__date', )
+
+    def draw_url(self, participant):
+        url = participant.simple_draw_url()
+        link = (
+            '<a href="http://confidentialsanta.co.nz{0}" target="_blank">'
+            'http://confidentialsanta.co.nz{0}'
+            '</a>'
+        ).format(url)
+        return mark_safe(link)
+    draw_url.short_description = 'Draw URL'
+
 admin.site.register(Participant, ParticipantAdmin)
 
 
